2495950eaf4d0c1c9c65976bfa10c5baad92bf66
Nikita Babko <niki-babko@yandex.ru>
2021-05-18 16:05:39 +0700

    Added use of hook useInput along with useValidation.

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,14 +1,24 @@
 import { useState } from 'react';
+import useValidation from './useValidation';
 
-export default function useInput(initialValue) {
+export default function useInput(initialValue, validations) {
   const [value, setValue] = useState(initialValue);
+  const [isDirty, setIsDirty] = useState(false);
+  const valid = useValidation(value, validations);
 
   const onChange = (e) => {
     setValue(e.target.value);
   };
 
+  const onBlur = (e) => {
+    setIsDirty(true);
+  };
+
   return {
     value,
     onChange,
+    onBlur,
+    isDirty,
+    ...valid,
   };
 }
